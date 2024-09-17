c3fdeaa9248cfc692053d3d75a7c36631c0804df
Senju <78903844+S1nju@users.noreply.github.com>
2024-09-17 19:38:50 +0100

    Update loading.js

--- src/components/loading.js
+++ src/components/loading.js
@@ -9,9 +9,24 @@ export default function Loading() {
   console.log(loading)
 
 useEffect(()=>{
+  const onPageLoad = () => {
+    console.log('page loaded');
+    setloading(false);
+    // do something else
+  };
 
-  setloading(false);
+  // Check if the page has already loaded
+  if (document.readyState === 'complete') {
+    onPageLoad();
+  } else {
+    window.addEventListener('load', onPageLoad, false);
+    // Remove the event listener when component unmounts
+    return () => window.removeEventListener('load', onPageLoad);
+  }
+
+
 },[])
+
 
 
 if(!loading){
